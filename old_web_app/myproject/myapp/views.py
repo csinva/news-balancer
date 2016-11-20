f82bdfbc12e00d459de47cbc9274a4cc2594cd64
Chandan <cs3hq@virginia.edu>
2016-11-19 22:55:43 -0500

    added glyphicons

--- old_web_app/myproject/myapp/views.py
+++ old_web_app/myproject/myapp/views.py
@@ -15,9 +15,9 @@ def list(request):
     docs_remaining = Document.objects.exclude(title=tit1).filter(cluster=cluster).order_by('bias')
     docs_count_3 = docs_remaining.count() / 3
     # objs = sorted(qs, key=lambda o: o.some_other_field)
-    docs_lib = sorted(docs_remaining[:docs_count_3], key=lambda x: x.cred)
-    docs_mid = sorted(docs_remaining[docs_count_3:2 * docs_count_3], key=lambda x: -1*x.cred)
-    docs_cons = docs_remaining[2 * docs_count_3:3 * docs_count_3]
+    docs_lib = sorted(docs_remaining[:docs_count_3], key=lambda x: -1 * x.cred)
+    docs_mid = sorted(docs_remaining[docs_count_3:2 * docs_count_3], key=lambda x: -1 * x.cred)
+    docs_cons = sorted(docs_remaining[2 * docs_count_3:3 * docs_count_3], key=lambda x: -1 * x.cred)
     print('num_docs_remaining', docs_count_3)
     # Render list page with the documents and the form
     return render_to_response(
